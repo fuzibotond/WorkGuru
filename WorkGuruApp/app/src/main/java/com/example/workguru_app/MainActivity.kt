6c012bfdbfc8c20e1ab8a98b74591d99249874d8
Füzi Botond <fuziboti@gmail.com>
2022-02-22 22:02:13 +0200

    Created splash screen  #9

--- WorkGuruApp/app/src/main/java/com/example/workguru_app/MainActivity.kt
+++ WorkGuruApp/app/src/main/java/com/example/workguru_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.workguru_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,4 +10,5 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_WorkGuruApp)
         setContentView(R.layout.activity_main)
     }
+
 }
