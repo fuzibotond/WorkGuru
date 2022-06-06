a4305a0d9c6ad1355c2833fa7de7270ce1cbd107
Füzi Botond <fuziboti@gmail.com>
2022-06-06 22:51:51 +0300

    #20 Send profile data to server

--- WorkGuruApp/app/src/main/java/com/license/workguru_app/profile/data/remote/DTO/ProfileRequest.kt
+++ WorkGuruApp/app/src/main/java/com/license/workguru_app/profile/data/remote/DTO/ProfileRequest.kt
@@ -1,11 +1,16 @@
 package com.license.workguru_app.profile.data.remote.DTO
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+
 
 data class ProfileRequest(
-    val city:String,
-    val street_address:String,
-    val country:String,
+    val city:String?,
+    val street_address:String?,
+    val country:String?,
     val avatar:String?,
-    val _method:String,
-    val state:String
+    val _method:String?,
+    val state:String?,
+    val delete_avatar:Boolean?
 )
